e7ef0a0e3a7a09fc0cc59f9830c062e06ec91816
ma-za-kpe <makpalyy@gmail.com>
2018-07-09 03:03:57 +0300

    set all current an scores to 0

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function Dice() {
   this.sides = 6;
   this.scores = [0,0];
   this.roundScore = 0;
-  this.activePlayer = 1;
+  this.activePlayer = 0;
   this.roll = function () {
     var diceNumber = Math.floor(Math.random()*this.sides) + 1;
     return diceNumber;
@@ -11,11 +11,16 @@ function Dice() {
 
 //ui logic
 $(document).ready(function () {
+  $("#current-0").text('0');
+  $("#current-1").text('0');
+  $("#score-0").text('0');
+  $("#score-1").text('0');
   $("button#roll").on('click', function(event) {
     var player1 = new Dice();
   //  var activePlayer = 0;
     $("#current-" + player1.activePlayer).text(player1.roll());
     $(".dice").css("display","block");
+    $(".dice").attr("src","images/dice-" + player1.roll() + ".png");
   });
 
 //  $("#current-"+ player1.activePlayer).val(" ");
